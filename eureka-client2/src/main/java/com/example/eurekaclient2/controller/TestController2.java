81cdd0c1689134609ed631adcf793a15e3b323dc
Denis0872 <denispetergof@gmail.com>
2023-11-17 16:19:40 +0300

    gateway

--- eureka-client2/src/main/java/com/example/eurekaclient2/controller/TestController2.java
+++ eureka-client2/src/main/java/com/example/eurekaclient2/controller/TestController2.java
@@ -1,13 +1,15 @@
 package com.example.eurekaclient2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/new")
 public class TestController2 {
 @GetMapping("/test2")
   public String  test(){
-        return "testing2";
+        return "hello Piter";
     }
 
 }
